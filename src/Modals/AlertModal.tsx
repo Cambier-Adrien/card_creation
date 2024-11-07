287cc6a5cad4368e26ca902126a56f723a7d60a0
Cambier-Adrien <adrien.cambier04@gmail.com>
2024-11-07 17:21:34 +0100

    07/11/2024

--- src/Modals/AlertModal.tsx
+++ src/Modals/AlertModal.tsx
@@ -1,4 +1,10 @@
+import {
+  faCheckCircle,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
 
 interface AlertModalProps {
   value: string;
@@ -12,24 +18,44 @@ export default function AlertModal({
   isError,
 }: AlertModalProps) {
   const body = document.querySelector("body")!;
-  return ReactDOM.createPortal(
-    <div
-      className={`${
-        isActive ? "flex" : "hidden"
-      } bottom-8 w-full flex justify-center fixed z-40`}
-    >
+  const [show, setShow] = useState(isActive);
+
+  useEffect(() => {
+    if (isActive) {
+      setShow(true);
+      const timer = setTimeout(() => setShow(false), 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [isActive]);
+
+  if (isActive) {
+    return ReactDOM.createPortal(
       <div
         className={`${
-          isError
-            ? "border-red-600 shadow-red-600/50"
-            : "border-teal-600 shadow-teal-600/50"
-        } shadow-2xl w-[30rem] px-8 p-4 rounded-xl bg-slate-950 border-2`}
+          show ? "opacity-100" : "opacity-0"
+        } transition-all duration-500 flex bottom-8 pr-8 w-full justify-end fixed z-40`}
       >
-        <div className="flex gap-2 justify-center items-center">
-          <p className="text-white font-['Inter'] text-sm">{value}</p>
+        <div className="bg-slate-900 border-slate-800 border shadow-lg p-4 px-8 rounded-xl w-fit">
+          <div className="flex gap-2 justify-center items-center">
+            {isError ? (
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                className="h-4 w-4 text-red-500"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="h-4 w-4 text-green-500"
+              />
+            )}
+
+            <p className="text-white font-['Inter'] text-sm">{value}</p>
+          </div>
         </div>
-      </div>
-    </div>,
-    body
-  );
+      </div>,
+      body
+    );
+  } else {
+    return null;
+  }
 }
